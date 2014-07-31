77721f68c33ef02038d50cef2524f5fbe250dbba
yegorov-p <yegorov.p@gmail.com>
2014-07-31 17:41:07 +0400

    Update zapretinfo.py
    Обновлено с учетом изменений API

--- zapretinfo.py
+++ zapretinfo.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "0.0.4"
+__version__ = "0.0.7"
 __author__ = "Pavel Yegorov"
 
 import suds
 from base64 import b64encode
 
-API_URL = "http://www.zapret-info.gov.ru/services/OperatorRequest/?wsdl"
+API_URL = "http://vigruzki.rkn.gov.ru/services/OperatorRequest/?wsdl"
+# API_URL = "http://vigruzki.rkn.gov.ru/services/OperatorRequestTest/?wsdl"
 
 class ZapretInfo:
+    def getLastDumpDateEx(self):
+        '''
+        Оставлен для совместимости. Аналогичен getLastDumpDateEx, но возвращает только один
+        параметр lastDumpDate.
+        '''
+        client = suds.client.Client(API_URL)
+        result=client.service.getLastDumpDateEx()
+        return result
+
     def getLastDumpDate(self):
+        '''
+        Метод предназначен для получения временной метки последнего обновления выгрузки из реестра,
+        а также для получения информации о версиях веб-сервиса, памятки и текущего формата выгрузки.
+        '''
         client = suds.client.Client(API_URL)
         result=client.service.getLastDumpDate()
         return result
 
-    def sendRequest(self,requestFile,signatureFile):
+    def sendRequest(self,requestFile,signatureFile,versionNum=2.0):
+        '''
+        Метод предназначен для направления запроса на получение выгрузки из реестра.
+        '''
         file = open(requestFile, "rb")
         data = file.read()
         file.close()
@@ -29,14 +46,17 @@ class ZapretInfo:
             sert = ''.join(data[1:-1])
         else:
             sert = ''.join(data)
-	sert = b64encode(sert)
-			
+
+        sert = b64encode(sert)
         client = suds.client.Client(API_URL)
-        result=client.service.sendRequest(xml,sert)
+        result=client.service.sendRequest(xml, sert, versionNum)
 
         return dict(((k, v.encode('utf-8')) if isinstance(v, suds.sax.text.Text) else (k, v)) for (k, v) in result)
 
     def getResult(self,code):
+        '''
+        Метод предназначен для получения результата обработки запроса - выгрузки из реестра
+        '''
         client = suds.client.Client(API_URL)
         result=client.service.getResult(code)
 
